2593eeda4b17b51a80f1930449575d3d043418cd
Hemant Pedanekar <hemantp@ti.com>
2009-08-11 11:12:22 +0530

    davinci: Use CONFIG_OSC_CLK_FREQ to set CLOCK_TICK_RATE

--- arch/arm/mach-davinci/include/mach/timex.h
+++ arch/arm/mach-davinci/include/mach/timex.h
@@ -11,7 +11,7 @@
 #ifndef __ASM_ARCH_TIMEX_H
 #define __ASM_ARCH_TIMEX_H
 
-/* The source frequency for the timers is the 27MHz clock */
-#define CLOCK_TICK_RATE 33000000
+/* The deafult source frequency for the timers is the 27MHz clock */
+#define CLOCK_TICK_RATE CONFIG_OSC_CLK_FREQ
 
 #endif /* __ASM_ARCH_TIMEX_H__ */
