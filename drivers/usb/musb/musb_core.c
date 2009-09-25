eebb2ed54b4d92ce08ced0a969325525410731c6
Ravi B <a0393587@psplinux051.india.ti.com>
2009-09-25 18:29:31 +0530

    usb: musb: update fifo table for davinci platforms for handling iso traffic
    This patch will update the fifo table for davinci platforms for
    handling iso transfers.
    
    Signed-off-by: Ravi B <a0393587@psplinux051.india.ti.com>

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -1066,11 +1066,11 @@ static struct fifo_cfg __initdata mode_2_cfg[] = {
 { .hw_ep_num = 1, .style = FIFO_TX,   .maxpacket = 512, },
 { .hw_ep_num = 1, .style = FIFO_RX,   .maxpacket = 512, },
 { .hw_ep_num = 2, .style = FIFO_TX,   .maxpacket = 512, },
-{ .hw_ep_num = 2, .style = FIFO_RX,   .maxpacket = 512, },
+{ .hw_ep_num = 2, .style = FIFO_RX,   .maxpacket = 1024, },
 { .hw_ep_num = 3, .style = FIFO_TX,   .maxpacket = 512, },
 { .hw_ep_num = 3, .style = FIFO_RX,   .maxpacket = 512, },
 { .hw_ep_num = 4, .style = FIFO_TX,   .maxpacket = 256, },
-{ .hw_ep_num = 4, .style = FIFO_RX,   .maxpacket = 256, },
+{ .hw_ep_num = 4, .style = FIFO_RX,   .maxpacket = 128, },
 };
 #endif
 
