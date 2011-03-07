5ecfe2417a03af423e0c7905176c0511290cb9a5
Raffaele Recalcati <raffaele.recalcati@bticino.it>
2011-03-07 18:48:45 +0100

    V4L/DVB: tda9885: driver converted to subdev
    Converting this driver to subdev device it allows
    to power it and configure it in an automatically way using
    s_stream v4l2 call coming from video_device.
    
    Signed-off-by: Raffaele Recalcati <raffaele.recalcati@bticino.it>

--- drivers/media/video/tda9885.c
+++ drivers/media/video/tda9885.c
@@ -2,7 +2,9 @@
  * Driver for the TDA9885 chip
  *
  * Copyright (c) 2010 Rodolfo Giometti <giometti@linux.it>
- * Copyright (c) 2010 cino S.p.A. <info@bticino.it>
+ * Copyright (c) 2011 Bticino S.p.A. <raffaele.recalcati@bticino.it>
+ * The former driver of Rodolfo Giometti has been converted
+ * to a subdev one.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -13,76 +15,201 @@
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/i2c.h>
+#include <linux/delay.h>
 #include <linux/i2c/tda9885.h>
+#include <linux/videodev2.h>
+#include <linux/gpio.h>
 
-#define DRIVER_VERSION	"1.0.0"
+#include <media/v4l2-device.h>
+#include <media/v4l2-i2c-drv.h>
 
-/*
- * Initialization function
- */
+#define DRIVER_VERSION	"2.0.0"
 
-static int tda9885_init_client(struct i2c_client *client,
-			struct tda9885_platform_data *pdata)
+#define AFCWIN (1<<7)
+
+static int debug; /* insmod parameter */
+module_param(debug, int, S_IRUGO | S_IWUSR);
+MODULE_LICENSE("GPL");
+
+struct tda9885 {
+	struct v4l2_subdev sd;
+	const struct tda9885_platform_data *pdata;
+	int status;
+};
+
+static inline struct tda9885 *to_state(struct v4l2_subdev *sd)
 {
-	u8 buf[] = {
-		0, pdata->switching_mode, pdata->adjust_mode, pdata->data_mode,
-	};
+	return container_of(sd, struct tda9885, sd);
+}
+
+static int tda9885_g_ctrl(struct v4l2_subdev *sd, struct v4l2_control *ctrl)
+{
+	v4l2_dbg(1, debug, sd, "%s : ctrl->id = %d\n", __func__, ctrl->id);
+	return 0;
+}
+
+static int tda9885_querystd(struct v4l2_subdev *sd, v4l2_std_id *std)
+{
+	struct i2c_client *client = v4l2_get_subdevdata(sd);
+	struct tda9885 *t = to_state(sd);
 	int ret;
+	u8 status;
+	u8 buf[] = {
+		0, t->pdata->switching_mode, t->pdata->adjust_mode, t->pdata->data_mode,
+	};
+
+	v4l2_dbg(1, debug, sd, "Switching ON the demodulator\n");
+	/* Power up */
+	gpio_set_value(t->pdata->power, 1);
+	mdelay(1);
 
 	/*
 	 * This chip is very simple, just write first the base address
 	 * and then all registers settings.
 	 */
 	ret = i2c_master_send(client, buf, ARRAY_SIZE(buf));
-	if (ret < 0)
-		return ret;
-	if (ret != ARRAY_SIZE(buf))
-		return -EIO;
+	ret = (ret == ARRAY_SIZE(buf)) ? 0 : ret;
+	v4l2_dbg(1, debug, sd, "Reading status byte\n");
+	ret = i2c_master_recv(client, &status, 1);
+	if (unlikely(ret != 1))
+		dev_err(&client->dev, "wanted %d bytes, got %d\n",
+			1, ret);
+	v4l2_dbg(1, debug, sd, "Status byte 0x%02X\n", status);
+	ret = 0;
 
+	switch (status & AFCWIN) {
+	case 1:
+		*std = V4L2_STD_PAL;
+	break;
+	case 0:
+	default:
+	break;
+	}
+
+	/* Power down */
+	gpio_set_value(t->pdata->power, 0);
 	return 0;
 }
+
+static int tda9885_s_stream(struct v4l2_subdev *sd, int enable)
+{
+	struct i2c_client *client = v4l2_get_subdevdata(sd);
+	struct tda9885 *t = to_state(sd);
+	int ret;
+	u8 buf[] = {
+		0, t->pdata->switching_mode, t->pdata->adjust_mode, t->pdata->data_mode,
+	};
+
+	switch (enable) {
+	case 0:
+	{
+		v4l2_dbg(1, debug, sd, "Switching OFF the demodulator\n");
+		/* Power Down */
+		gpio_set_value(t->pdata->power, 0);
+		ret = 0;
+		break;
+	}
+	case 1:
+	{
+		v4l2_dbg(1, debug, sd, "Switching ON the demodulator\n");
+		/* Power up */
+		gpio_set_value(t->pdata->power, 1);
+
+		/*
+		 * Little delay for power up
+		 * datasheet: time constant (R × C) for network
+		 * without i2c bus is 1.2 usec
+		*/
+		mdelay(1);
+
+		/*
+		 * This chip is very simple, just write first the base address
+		 * and then all registers settings.
+		 */
+		ret = i2c_master_send(client, buf, ARRAY_SIZE(buf));
+		ret = (ret == ARRAY_SIZE(buf)) ? 0 : ret;
+
+		v4l2_dbg(1, debug, sd, "Reading status byte\n");
+		ret = i2c_master_recv(client, buf, 1);
+		if (unlikely(ret != 1))
+			dev_err(&client->dev, "wanted %d bytes, got %d\n",
+				1, ret);
+		v4l2_dbg(1, debug, sd, "Status byte 0x%02X\n", buf[0]);
+		ret = 0;
+		break;
+	}
+	default:
+		return -ENODEV;
+		break;
+	}
+
+	return ret;
+}
+
+static const struct v4l2_subdev_video_ops tda9885_video_ops = {
+	.s_stream = tda9885_s_stream,
+	.querystd = tda9885_querystd,
+};
+
+static const struct v4l2_subdev_core_ops tda9885_core_ops = {
+	.g_ctrl = tda9885_g_ctrl,
+};
+
+static const struct v4l2_subdev_ops tda9885_ops = {
+	.core = &tda9885_core_ops,
+	.video = &tda9885_video_ops
+};
 
 /*
  * I2C init/probing/exit functions
  */
-
-static int __devinit tda9885_probe(struct i2c_client *client,
+static int tda9885_probe(struct i2c_client *client,
 			const struct i2c_device_id *id)
 {
-	struct i2c_adapter *adapter = to_i2c_adapter(client->dev.parent);
-	struct tda9885_platform_data *pdata = client->dev.platform_data;
+	struct tda9885 *data;
+	struct v4l2_subdev *sd;
 	int err = 0;
 
-	if (!i2c_check_functionality(adapter, I2C_FUNC_SMBUS_BYTE)) {
+	v4l_info(client, "chip found @ 0x%02x (%s)\n",
+			client->addr << 1, client->adapter->name);
+
+	if (!i2c_check_functionality(client->adapter, I2C_FUNC_SMBUS_BYTE)) {
 		err = -EIO;
 		goto exit;
 	}
 
-	/* No private data for this driver */
-	i2c_set_clientdata(client, NULL);
-
-	/* Check platform data */
-	if (!pdata)
-		dev_warn(&client->dev, "no default init data");
-	else {
-		dev_info(&client->dev, "default switching mode is %02x\n",
-				pdata->switching_mode);
-		dev_info(&client->dev, "default adjust mode is %02x\n",
-				pdata->adjust_mode);
-		dev_info(&client->dev, "default data mode is %02x\n",
-				pdata->data_mode);
-
-		/* Initialize the TDA9885 chip */
-		err = tda9885_init_client(client, pdata);
-		if (err)
-			goto exit;
+	if (!client->dev.platform_data) {
+		v4l2_err(client, "No platform data!!\n");
+		return -ENODEV;
 	}
 
-	/*
-	 * This driver doesn't need any settings once it's initialized,
-	 * so no sysfs hooks are needed to manage it...
-	 */
-	dev_info(&client->dev, "support ver. %s enabled\n", DRIVER_VERSION);
+	data = kzalloc(sizeof(struct tda9885), GFP_KERNEL);
+	if (!data) {
+		err = -ENOMEM;
+		goto exit;
+	} else {
+		/* Copy board specific information here */
+		data->pdata = client->dev.platform_data;
+
+		i2c_set_clientdata(client, data);
+
+			}
+
+	/* switching off the demodulator */
+	gpio_set_value(data->pdata->power, 0);
+
+	/* Register with V4L2 layer as slave device */
+	sd = &data->sd;
+	v4l2_i2c_subdev_init(sd, client, &tda9885_ops);
+	v4l2_dbg(1, debug, sd, "default switching mode is 0x%02x\n",
+		data->pdata->switching_mode);
+	v4l2_dbg(1, debug, sd, "default adjust mode is 0x%02x\n",
+		data->pdata->adjust_mode);
+	v4l2_dbg(1, debug, sd, "default data mode is 0x%02x\n",
+		data->pdata->data_mode);
+	v4l2_dbg(1, debug, sd, "power gpio is %d\n",
+		data->pdata->power);
+	v4l2_info(sd, "%s decoder driver registered (ver. %s)\n", sd->name, DRIVER_VERSION);
 
 	return 0;
 
@@ -90,10 +217,16 @@ exit:
 	return err;
 }
 
-static int __devexit tda9885_remove(struct i2c_client *client)
+static int tda9885_remove(struct i2c_client *client)
 {
-	/* Nop! =:-o */
+	struct v4l2_subdev *sd = i2c_get_clientdata(client);
+	struct tda9885 *t = to_state(sd);
 
+	v4l2_device_unregister_subdev(sd);
+
+	/* it should be already off */
+	gpio_set_value(t->pdata->power, 0);
+	kfree(to_state(sd));
 	return 0;
 }
 
@@ -103,25 +236,13 @@ static const struct i2c_device_id tda9885_id[] = {
 };
 MODULE_DEVICE_TABLE(i2c, tda9885_id);
 
-static struct i2c_driver tda9885_driver = {
-	.driver = {
-		.name   = "tda9885",
-		.owner  = THIS_MODULE,
-	},
+static struct v4l2_i2c_driver_data v4l2_i2c_data = {
+	.name   = "tda9885",
 	.probe = tda9885_probe,
-	.remove = __devexit_p(tda9885_remove),
+	.remove = tda9885_remove,
 	.id_table = tda9885_id,
 };
 
-static int __init tda9885_init(void)
-{
-	return i2c_add_driver(&tda9885_driver);
-}
-
-static void __exit tda9885_exit(void)
-{
-	i2c_del_driver(&tda9885_driver);
-}
 
 /* Module information */
 MODULE_AUTHOR("Rodolfo Giometti <giometti@linux.it>");
@@ -129,5 +250,3 @@ MODULE_DESCRIPTION("TDA9885 IF-PPL demodulator driver");
 MODULE_LICENSE("GPLv2");
 MODULE_VERSION(DRIVER_VERSION);
 
-module_init(tda9885_init);
-module_exit(tda9885_exit);
--- include/linux/i2c/tda9885.h
+++ include/linux/i2c/tda9885.h
@@ -5,6 +5,7 @@ struct tda9885_platform_data {
 	u8 switching_mode;
 	u8 adjust_mode;
 	u8 data_mode;
+	int power;
 };
 
 #endif /* __LINUX_I2C_TDA9885_H */
