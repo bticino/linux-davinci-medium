84f0b6514b14d12504b91c496d03af3001dedcd5
Fabio Mauri <fabio.mauri@bticino.it>
2011-04-19 10:34:55 +0200

    DaVinci: Dingo Board: fixed mac address eeprom offset
    Signed-off-by: Fabio Mauri <fabio.mauri@bticino.it>

--- arch/arm/mach-davinci/board-dingo.c
+++ arch/arm/mach-davinci/board-dingo.c
@@ -110,7 +110,7 @@ static struct at24_platform_data at24_info = {
 	.page_size = 64,
 	.flags = AT24_FLAG_ADDR16,
 	.setup = davinci_get_mac_addr,
-	.context = (void *)0x7f00,	/* where it gets the mac-address */
+	.context = (void *)0x19e,	/* where it gets the mac-address */
 };
 
 static struct timer_list pf_debounce_timer;
