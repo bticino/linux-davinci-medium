9bc9dddc31a6cd5c14c22bac3c9f77ccc3d35be2
Davide Bonfanti <davide.bonfanti@bticino.it>
2013-06-12 15:39:11 +0200

    ARM: davinci: map default_queue to edma channels
    Default queue is expected to be a low-priority queue.
    This way, long transfers on the default queue started
    by the codec engine will not cause audio defects.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>
    Signed-off-by: Juha Kuikka <juha.kuikka@gmail.com>
    Reported-by: Juha Kuikka <juha.kuikka@gmail.com>
    Acked-by: Rajashekhara, Sudhakar <sudhakar.raj@ti.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>
    Signed-off-by: Davide Bonfanti <davide.bonfanti@bticino.it>

--- arch/arm/mach-davinci/include/mach/edma.h
+++ arch/arm/mach-davinci/include/mach/edma.h
@@ -278,6 +278,11 @@ struct edma_soc_info {
 	unsigned	n_slot;
 	unsigned	n_tc;
 	unsigned	n_cc;
+	/*
+	 * Default queue is expected to be a low-priority queue.
+	 * This way, long transfers on the default queue started
+	 * by the codec engine will not cause audio defects.
+	 */
 	enum dma_event_q	default_queue;
 
 	const s8	(*rsv_chans)[2];
